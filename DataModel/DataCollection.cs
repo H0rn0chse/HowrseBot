73f542ef0836b7de27d12cc0cb311b3f6ca71ea9
H0rn0chse <h0rn0chse@gmx.de>
2017-09-02 18:59:34 +0200

    Removed some Testfunctions

--- DataModel/DataCollection.cs
+++ DataModel/DataCollection.cs
@@ -18,63 +18,13 @@ namespace HowrseBot.DataModel
         public ObservableCollection<Item> Duration { get; set; } = new ObservableCollection<Item>();
         public ObservableCollection<Item> Sort { get; set; } = new ObservableCollection<Item>();
         public ObservableCollection<Item> Tarif { get; set; } = new ObservableCollection<Item>();
-
-        public ObservableCollection<BotProgram> ProgramList = new ObservableCollection<BotProgram>();
-        public ObservableCollection<Item> Prog1 { get; set; } = new ObservableCollection<Item>();
-		public ObservableCollection<Test> Test { get; set; } = new ObservableCollection<Test>();
         
-        
-        /*
-        public static List<Tarif> TarifList;
-        public static List<Duration> DurationList;
-        public static List<Sort> SortList;
-        public static List<string> AcceptedUserNames;
-        public static List<UriNamePair> UriList;
-        */
         public DataCollection()
 		{
             CommandPool = StaticData.Instance.GetItemList("CommandPool");
             Duration = StaticData.Instance.GetItemList("Duration");
             Sort = StaticData.Instance.GetItemList("Sort");
             Tarif = StaticData.Instance.GetItemList("Tarif");
-
-
-            for (int i=1; i < 6; i++)
-			{
-				Item _item = new Item(i);
-				Prog1.Add(_item);
-				Test _test = new Test(i);
-				Test.Add(_test);
-			}
-
 		}
-
-	}
-
-}
-
-public class Test : INotifyPropertyChanged
-{
-	public Test()
-	{
-	}
-
-	public Test(int itemIndex) : this()
-    {
-		this.Breed = $"Zucht {itemIndex}";
-		this.Program = $"Programm {itemIndex}";
-		this.Progress = itemIndex*16;
-	}
-
-	public string Breed { get; set; }
-	public string Program { get; set; }
-	public int Progress { get; set; }
-
-	public event PropertyChangedEventHandler PropertyChanged;
-
-	[NotifyPropertyChangedInvocator]
-	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 }
